2d510534acd92ed581d3f83818e6e175cb031883
vinu joy <vinujoy2010@gmail.com>
2020-04-12 22:42:23 +0530

    input form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignINSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path ='/signin' component ={SignINSignUp}></Route>
       </Switch>
     </div>
   );
